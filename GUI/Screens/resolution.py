bc5dbaabb7af74edf8dccaa32700961f9443e034
Paul Valerie GOMA <paulvalerie.goma@edu.ece.fr>
2024-08-02 14:53:51 +0200

    add some functions

---
+++ GUI/Screens/resolution.py
@@ -0,0 +1,42 @@
+"""
+    # This file contains all the functions needed to manage screen resolution and resizing. 
+"""
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QSize, QSizeF
+
+# ::::::::Screen size recovery function::::::::::::: #
+# screen resolution in pixel
+def getResolutions() -> QSize:
+    app = QApplication.instance() 
+    if app is None:
+        app = QApplication(sys.argv)
+    return app.primaryScreen().size()
+
+# Physical screen dimensions (width/height)
+def getScreenPhysicalSize() -> QSizeF:
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+    return app.primaryScreen().physicalSize()
+
+# Pixel per inch
+def getDpi() -> float:
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+    return app.primaryScreen().logicalDotsPerInch()
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# ::::::::Responsive functions::::::::::::: #
+# Keep proportions
+
+
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# ::::::::display window in center of screen::::::::::::: #
+def centerWindow(window: QMainWindow) -> None:
+    x = (getResolutions().width() - window.width()) // 2
+    y = (getResolutions().height() - window.height()) // 2
+
+    window.move(x, y)
